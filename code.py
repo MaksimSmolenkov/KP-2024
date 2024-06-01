41609d279b8ba9c81372db6f07d646acf550030a
MaksimSmolenkov <125339609+MaksimSmolenkov@users.noreply.github.com>
2024-06-01 17:37:39 +0600

    Update code.py

--- code.py
+++ code.py
@@ -1,217 +1,154 @@
-// Реализация описанного подхода на C#
-using System;
-using System.Collections.Generic;
-using System.Linq;
-// Структура GNOME
-// строка определяет путь, пройденный
-// коммивояжером, в то время как значение
-// приспособленности пути хранится в целочисленной переменной
-public struct Individual
-{
-    public string gnome;
-    public int fitness;
-}
+from random import randint, sample
 
-public class TSP
-{
-// количесво городов
-    const int V = 5;
-    // названия городов
-    const string GENES = "ABCDE";
-    // Значение начального узла
-    const int START = 0;
-    // Начальный размер популяции для алгоритма
-    const int POP_SIZE = 10;
+INT_MAX = 2147483647
+V = 5  # Количество городов
+GENES = "ABCDE"  # Имена городов
+START = 0  # Начальный город
+POP_SIZE = 10  # Размер популяции
 
-// Функция для возврата случайного числа
-// в диапазоне от start до end
-    static int RandNum(int start, int end)
-    {
-        int r = end - start;
-        int rnum = start + new Random().Next() % r;
-        return rnum;
-    }
+class Individual:
+    def __init__(self) -> None:
+        self.gnome = ""
+        self.fitness = 0
 
-// Функция для проверки, встречался ли символ
-// уже в строке
-    static bool Repeat(string s, char ch)
-    {
-        for (int i = 0; i < s.Length; i++)
-        {
-            if (s[i] == ch)
-                return true;
-        }
-        return false;
-    }
-// Функция для возврата мутировавшего GNOME
-// Мутировавший GNOME - это строка
-// с случайной перестановкой
-// двух генов для создания разнообразия в видах
-    static string MutatedGene(string gnome)
-    {
-        while (true)
-        {
-            int r = RandNum(1, V);
-            int r1 = RandNum(1, V);
-            if (r1 != r)
-            {
-                char[] arr = gnome.ToCharArray();
-                char temp = arr[r];
-                arr[r] = arr[r1];
-                arr[r1] = temp;
-                gnome = new string(arr);
-                break;
-            }
-        }
-        return gnome;
-    }
+    def __lt__(self, other):
+        return self.fitness < other.fitness
 
-// Функция для возврата допустимой строки GNOME
-// необходимой для создания популяции
-    static string CreateGnome()
-    {
-        string gnome = "0";
-        while (true)
-        {
-            if (gnome.Length == V)
-            {
-                gnome += gnome[0];
-                break;
-            }
-            int temp = RandNum(1, V);
-            if (!Repeat(gnome, (char)(temp + 48)))
-                gnome += (char)(temp + 48);
-        }
-        return gnome;
-    }
+    def __gt__(self, other):
+        return self.fitness > other.fitness
 
-// Функция для возврата значения приспособленности GNOME.
-// Значение приспособленности - длина пути
-// представленного gnome.
-    static int CalFitness(string gnome)
-    {
-        int[,] map = new int[,] {
-            { 0, 2, int.MaxValue, 12, 5 },
-            { 2, 0, 4, 8, int.MaxValue },
-            { int.MaxValue, 4, 0, 3, 3 },
-            { 12, 8, 3, 0, 10 },
-            { 5, int.MaxValue, 3, 10, 0 }
-        };
-        int f = 0;
-        for (int i = 0; i < gnome.Length - 1; i++)
-        {
-            if (map[gnome[i] - 48, gnome[i + 1] - 48] == int.MaxValue)
-                return int.MaxValue;
-            f += map[gnome[i] - 48, gnome[i + 1] - 48];
-        }
-        return f;
-    }
+# Функция для генерации случайного числа в заданном диапазоне
+def rand_num(start, end):
+    return randint(start, end-1)
 
-// Функция для возврата обновленного значения
-    static int CoolDown(int temp)
-    {
-        return (90 * temp) / 100;
-    }
-// Компаратор для структуры GNOME.
-    static bool LessThan(Individual t1, Individual t2)
-    {
-        return t1.fitness < t2.fitness;
-    }
+# Функция для проверки, повторяется ли символ в строке
+def repeat(s, ch):
+    return ch in s
 
-   
-        // Вспомогательная функция для задачи коммивояжера.
-        static void TSPUtil(int[,] map)
-        {
-            
-            // Номер поколения
-            int gen = 1;
-            
-            // Количество итераций генов
-            int gen_thres = 5;
+# Функция для мутации генома (случайный обмен двух генов)
+def mutated_gene(gnome):
+    gnome = list(gnome)
+    while True:
+        r = rand_num(1, V)
+        r1 = rand_num(1, V)
+        if r1 != r:
+            gnome[r], gnome[r1] = gnome[r1], gnome[r]
+            break
+    return ''.join(gnome)
 
-            List<Individual> population = new List<Individual>();
-            Individual temp;
+# Функция для создания начального генома
+def create_gnome():
+    gnome = "0"
+    while True:
+        if len(gnome) == V:
+            gnome += gnome[0]
+            break
 
-            // Формирование пула GNOME.
-            for (int i = 0; i < POP_SIZE; i++)
-            {
-                temp.gnome = CreateGnome();
-                temp.fitness = CalFitness(temp.gnome);
-                population.Add(temp);
-            }
+        temp = rand_num(1, V)
+        if not repeat(gnome, chr(temp + 48)):
+            gnome += chr(temp + 48)
 
-            Console.WriteLine("\nInitial population: \nGNOME     FITNESS VALUE\n");
-            foreach (Individual ind in population)
-            {
-                Console.WriteLine(ind.gnome + " " + ind.fitness);
-            }
-            Console.WriteLine();
+    return gnome
 
-            bool found = false;
-            int temperature = 10000;
+# Функция для вычисления приспособленности (длины пути)
+def cal_fitness(gnome, mp):
+    f = 0
+    for i in range(len(gnome) - 1):
+        if mp[ord(gnome[i]) - 48][ord(gnome[i + 1]) - 48] == INT_MAX:
+            return INT_MAX
+        f += mp[ord(gnome[i]) - 48][ord(gnome[i + 1]) - 48]
+    return f
 
-            // Итерация для выполнения скрещивания популяции и мутации генов.
-            while (temperature > 1000 && gen <= gen_thres)
-            {
-                population = population.OrderBy(x => x.fitness).ToList();
-                Console.WriteLine("\nCurrent temp: " + temperature + "\n");
-                List<Individual> new_population = new List<Individual>();
+# Функция для снижения температуры
+def cooldown(temp):
+    return (90 * temp) / 100
 
-                for (int i = 0; i < POP_SIZE; i++)
-                {
-                    Individual p1 = population[i];
+# Функция для кроссовера (рекомбинации) двух родителей
+def crossover(parent1, parent2):
+    child_p1 = ""
+    child_p2 = ""
 
-                    while (true)
-                    {
-                        string new_g = MutatedGene(p1.gnome);
-                        Individual new_gnome;
-                        new_gnome.gnome = new_g;
-                        new_gnome.fitness = CalFitness(new_gnome.gnome);
+    geneA = int(rand_num(1, V) * len(parent1))
+    geneB = int(rand_num(1, V) * len(parent1))
 
-                        if (new_gnome.fitness <= population[i].fitness)
-                        {
-                            new_population.Add(new_gnome);
-                            break;
-                        }
-                        else
-                        {
+    startGene = min(geneA, geneB)
+    endGene = max(geneA, geneB)
 
-                            
-                            // Принятие отвергнутых потомков
-                            // с возможной вероятностью выше порога.
-                            float prob = (float)Math.Pow(2.7,
-                                            -1 * ((float)(new_gnome.fitness
-                                                    - population[i].fitness)
-                                                / temperature));
-                            if (prob > 0.5)
-                            {
-                                new_population.Add(new_gnome);
-                                break;
-                            }
-                        }
-                    }
-                }
+    for i in range(startGene, endGene):
+        if i < len(parent1):  # Проверяем, не вышли ли за границы длины генома
+            child_p1 += parent1[i]
 
-                temperature = CoolDown(temperature);
-                population = new_population;
-                Console.WriteLine("Generation " + gen + " \nGNOME     FITNESS VALUE\n");
+    child_p2 = [item for item in parent2 if item not in child_p1]
 
-                foreach (Individual ind in population)
-                {
-                    Console.WriteLine(ind.gnome + " " + ind.fitness);
-                }
-                gen++;
-            }
-        }
+    child = child_p1 + ''.join(child_p2)
+    return child
 
-        static void Main(string[] args)
-        {
-            int[,] map = new int[,] { { 0, 2, int.MaxValue, 12, 5 },
-                                      { 2, 0, 4, 8, int.MaxValue },
-                                      { int.MaxValue, 4, 0, 3, 3 },
-                                      { 12, 8, 3, 0, 10 },
-                                      { 5, int.MaxValue, 3, 10, 0 } };
+# Основная функция, реализующая генетический алгоритм для TSP
+def TSPUtil(mp):
+    gen = 1
+    gen_thres = 100
 
-            TSPUtil(map);
-        }
-}
+    # Инициализация популяции
+    population = []
+    best_gnome = None
+    best_fitness = float('inf')
+
+    for _ in range(POP_SIZE):
+        temp = Individual()
+        temp.gnome = create_gnome()
+        temp.fitness = cal_fitness(temp.gnome, mp)
+        population.append(temp)
+
+    print("\nInitial population: \nGNOME     FITNESS VALUE\n")
+    for ind in population:
+        print(ind.gnome, ind.fitness)
+        if ind.fitness < best_fitness:
+            best_fitness = ind.fitness
+            best_gnome = ind.gnome
+    print("Best Gnome in Generation 0:", best_gnome, "Length:", best_fitness)
+
+    temperature = 10000
+
+    # Основной цикл генетического алгоритма
+    while temperature > 1000 and gen <= gen_thres:
+        population.sort()  # Сортировка популяции по приспособленности
+        print("\nCurrent temp: ", temperature)
+        new_population = []
+
+        for i in range(POP_SIZE):
+            parent1 = population[rand_num(0, POP_SIZE)].gnome
+            parent2 = population[rand_num(0, POP_SIZE)].gnome
+            child_gnome = crossover(parent1, parent2)  # Кроссовер
+
+            if rand_num(0, 10) < 5:
+                child_gnome = mutated_gene(child_gnome)  # Мутация
+
+            new_gnome = Individual()
+            new_gnome.gnome = child_gnome
+            new_gnome.fitness = cal_fitness(new_gnome.gnome, mp)
+
+            new_population.append(new_gnome)
+
+        temperature = cooldown(temperature)  # Снижение температуры
+        population = new_population
+        print("Generation", gen)
+        print("GNOME     FITNESS VALUE")
+        best_gnome = None
+        best_fitness = float('inf')
+        for ind in population:
+            print(ind.gnome, ind.fitness)
+            if ind.fitness < best_fitness:
+                best_fitness = ind.fitness
+                best_gnome = ind.gnome
+        print("Best Gnome in Generation", gen, ":", best_gnome, "Length:", best_fitness)
+        gen += 1
+
+if __name__ == "__main__":
+    mp = [
+        [0, 2, INT_MAX, 12, 5],
+        [2, 0, 4, 8, INT_MAX],
+        [INT_MAX, 4, 0, 3, 3],
+        [12, 8, 3, 0, 10],
+        [5, INT_MAX, 3, 10, 0],
+    ]
+    TSPUtil(mp)
